299e8b084bd8a2cb24eea43fbc5688ee178eb468
KJ Price <kjprice12@gmail.com>
2023-01-21 10:12:37 -0600

    Create setup scripts

--- scrape/bin/misc/install.sh
+++ scrape/bin/misc/install.sh
@@ -1,1 +1,9 @@
+cd "$(dirname "$0")"
+
 conda create -y -n scrape python=3.9
+
+nvm install v19.4.0
+
+if [ ! -d ]; then 
+( cd js && npx create-react-app scrape_frontend )
+fi
--- scrape/bin/misc/setup_shell.sh
+++ scrape/bin/misc/setup_shell.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 conda activate scrape
+nvm use v19.4.0
